3cc4d4c4f0a5b67413a0df6a6dee212bd8d6e351
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-01 20:31:20 +0100

    Added users.txt

--- src/main/java/com/clantags/impl/ClanTagsImpl.java
+++ src/main/java/com/clantags/impl/ClanTagsImpl.java
@@ -1,5 +1,20 @@
 package com.clantags.impl;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClanTagsImpl
 {
+	public ClanTagsImpl()
+	{
+	}
+	
+	/**
+	 * Fetches users from the database
+	 * @return whether or not it was successful
+	 */
+	public boolean fetchUsers() throws MalformedURLException
+	{
+		final URL dbLoc = new URL("");
+	}
 }
