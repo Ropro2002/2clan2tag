f0215fbea4a560eb1bcf6a34ea2e7c3b7da7bead
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-01 20:26:07 +0100

    Added main class

--- src/main/java/com/clantags/ClanTags.java
+++ src/main/java/com/clantags/ClanTags.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.clantags;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = com.clantags.ClanTags.MODID, name = com.clantags.ClanTags.NAME, version = com.clantags.ClanTags.VERSION)
+public class ClanTags
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "clantags";
+    public static final String NAME = "2Clan2Tags";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
---
+++ src/main/java/com/clantags/api/ClanTagApi.java
@@ -0,0 +1,5 @@
+package com.clantags.api;
+
+public class ClanTagApi
+{
+}
