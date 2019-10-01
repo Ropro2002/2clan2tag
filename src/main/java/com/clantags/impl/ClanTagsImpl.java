1111a1c9bc9986ecedc0208d800a14dea565aa6c
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-01 23:13:15 +0100

    Added option for multiple databases

--- src/main/java/com/clantags/impl/ClanTagsImpl.java
+++ src/main/java/com/clantags/impl/ClanTagsImpl.java
@@ -21,34 +21,35 @@ public class ClanTagsImpl
 	
 	/**
 	 * Fetches users from the database
-	 * @throws MalformedURLException (should not occur in production)
 	 * @throws IOException If an error occurs while loading
 	 */
-	public void fetchUsers() throws MalformedURLException, IOException
+	public void fetchUsers(URL[] databases) throws IOException
 	{
-		// TODO: Remove token before release, needed so that we can access the raw content in a private repo
-		final URL dbLoc = new URL("https://raw.githubusercontent.com/Ropro2002/2clan2tag/master/api/users.txt?token=AM26DG3FCBNKGK5YWS5TYKC5TTSBK");
-		
-		BufferedReader in = new BufferedReader(
-			new InputStreamReader(
-				dbLoc.openStream()
-			)
-		);
-		
 		factionMembers = new HashMap<>();
 		
-		String line;
-		while((line = in.readLine()) != null)
+		for(URL dbLoc : databases)
 		{
-			String[] splitLine = line.split(Pattern.quote(":"));
-			if(splitLine.length == 2)
+			ClanTags.logger.info("Fetching users from database '" + dbLoc + "'");
+			
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+					dbLoc.openStream()
+				)
+			);
+			
+			String line;
+			while((line = in.readLine()) != null)
 			{
-				factionMembers.put(splitLine[0], splitLine[1]);
+				String[] splitLine = line.split(Pattern.quote(":"));
+				if(splitLine.length == 2)
+				{
+					factionMembers.put(splitLine[0], splitLine[1]);
+				}
 			}
+			
+			ClanTags.logger.info("Found " + factionMembers.size() + " faction members");
+			
+			in.close();
 		}
-		
-		ClanTags.logger.info("Found " + factionMembers.size() + " faction members");
-		
-		in.close();
 	}
 }
