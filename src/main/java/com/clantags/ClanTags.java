88d185617ef6d27b259cff60a95f3163973a41c7
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-01 20:38:51 +0100

    Added fetchusers

--- src/main/java/com/clantags/ClanTags.java
+++ src/main/java/com/clantags/ClanTags.java
@@ -14,7 +14,7 @@ public class ClanTags
     public static final String NAME = "2Clan2Tags";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
--- src/main/java/com/clantags/impl/ClanTagsImpl.java
+++ src/main/java/com/clantags/impl/ClanTagsImpl.java
@@ -1,7 +1,15 @@
 package com.clantags.impl;
 
+import com.clantags.ClanTags;
+import net.minecraftforge.fml.common.FMLLog;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class ClanTagsImpl
 {
@@ -9,12 +17,38 @@ public class ClanTagsImpl
 	{
 	}
 	
+	private HashMap<String, String> factionMembers = null;
+	
 	/**
 	 * Fetches users from the database
-	 * @return whether or not it was successful
+	 * @throws MalformedURLException (should not occur in production)
+	 * @throws IOException If an error occurs while loading
 	 */
-	public boolean fetchUsers() throws MalformedURLException
+	public void fetchUsers() throws MalformedURLException, IOException
 	{
-		final URL dbLoc = new URL("");
+		// TODO: Remove token before release, needed so that we can access the raw content in a private repo
+		final URL dbLoc = new URL("https://raw.githubusercontent.com/Ropro2002/2clan2tag/master/api/users.txt?token=AM26DG3FCBNKGK5YWS5TYKC5TTSBK");
+		
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(
+				dbLoc.openStream()
+			)
+		);
+		
+		factionMembers = new HashMap<>();
+		
+		String line;
+		while((line = in.readLine()) != null)
+		{
+			String[] splitLine = line.split(Pattern.quote(":"));
+			if(splitLine.length == 2)
+			{
+				factionMembers.put(splitLine[0], splitLine[1]);
+			}
+		}
+		
+		ClanTags.logger.info("Found " + factionMembers.size() + " faction members");
+		
+		in.close();
 	}
 }
