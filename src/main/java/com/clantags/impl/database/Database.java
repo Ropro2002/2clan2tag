75dbe0110186b124b937ed30d9cc92e343552db0
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-02 13:06:27 +0100

    Finished simple database

--- src/main/java/com/clantags/impl/database/Database.java
+++ src/main/java/com/clantags/impl/database/Database.java
@@ -1,12 +1,18 @@
 package com.clantags.impl.database;
 
-import com.clantags.impl.Clan;
+import com.clantags.impl.factions.Faction;
 import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.HashMap;
+import java.util.List;
 
 public abstract class Database
 {
 	public String dbName;
 	public String dbFormat;
+	public List<Faction> factions;
 	
-	public abstract Clan getPlayerClan(EntityPlayer player);
+	protected HashMap<String, Faction> playerCache;
+	
+	public abstract Faction getPlayerClan(EntityPlayer player);
 }
--- src/main/java/com/clantags/impl/database/SimpleDatabase.java
+++ src/main/java/com/clantags/impl/database/SimpleDatabase.java
@@ -1,18 +1,49 @@
 package com.clantags.impl.database;
 
-import com.clantags.impl.Clan;
+import com.clantags.impl.factions.Faction;
 import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 public class SimpleDatabase extends Database
 {
-	public SimpleDatabase(String dbName, String dbFormat, )
+	public SimpleDatabase(String dbName, String dbFormat, List<Faction> factions)
 	{
-	
+		this.dbName = dbName;
+		this.dbFormat = dbFormat;
+		this.factions = factions;
+		
+		this.playerCache = new HashMap<>();
 	}
 	
 	@Override
-	public Clan getPlayerClan(EntityPlayer player)
+	public Faction getPlayerClan(EntityPlayer player)
 	{
+		// If this player is in the cache then return that
+		try
+		{
+			return Objects.requireNonNull(playerCache.get(player.getName()));
+		} catch(Exception ignored){}
+		
+		for(Faction faction : factions)
+		{
+			for(String member : faction.members)
+			{
+				if(member.equalsIgnoreCase(player.getName()))
+				{
+					// The player is a member of this clan
+					
+					// Add them to the cache
+					playerCache.put(member, faction);
+					
+					return faction;
+				}
+			}
+		}
+		
+		// No faction was found
 		return null;
 	}
 }
