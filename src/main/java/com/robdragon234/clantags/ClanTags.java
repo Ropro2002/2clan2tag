0d1c02202a3686c925e7247b410e9081ffc9e99a
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-02 21:12:44 +0100

    Fixed tabs in advanced db

--- src/main/java/com/robdragon234/clantags/ClanTags.java
+++ src/main/java/com/robdragon234/clantags/ClanTags.java
@@ -1,5 +1,6 @@
 package com.robdragon234.clantags;
 
+import com.robdragon234.clantags.impl.ClanTagsImpl;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -13,12 +14,13 @@ import org.apache.logging.log4j.Logger;
         name = ClanTags.MOD_NAME,
         version = ClanTags.VERSION
 )
-public class ClanTags {
-
+public class ClanTags
+{
     public static final String MOD_ID = "clantags";
     public static final String MOD_NAME = "Clantags";
     public static final String VERSION = "2019.2-1.3.1";
 
+    public static ClanTagsImpl clanTagsImpl;
     public static Logger logger;
 
     @Mod.Instance(MOD_ID)
@@ -26,17 +28,21 @@ public class ClanTags {
 
 
     @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
+    public void preinit(FMLPreInitializationEvent event)
+    {
         logger = event.getModLog();
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void init(FMLInitializationEvent event)
+    {
         logger.info("Initialising 2clan2tags...");
     }
 
     @Mod.EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
-
+    public void postinit(FMLPostInitializationEvent event)
+    {
+        clanTagsImpl = new ClanTagsImpl();
+        clanTagsImpl.fetchUsers();
     }
 }
