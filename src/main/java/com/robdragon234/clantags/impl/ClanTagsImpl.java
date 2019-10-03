a239277a7198b2671ffad9790b57033e29aa90fb
cookiedragon234 <cookie.dragon234@gmail.com>
2019-10-03 14:32:59 +0100

    Created setNameTag and getFaction

--- src/main/java/com/robdragon234/clantags/impl/ClanTagsImpl.java
+++ src/main/java/com/robdragon234/clantags/impl/ClanTagsImpl.java
@@ -6,8 +6,11 @@ import com.robdragon234.clantags.impl.factions.Faction;
 import com.robdragon234.clantags.impl.members.Member;
 import com.robdragon234.clantags.impl.parser.DatabaseParser;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.FMLLog;
 
+import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,6 +19,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class ClanTagsImpl
@@ -24,7 +28,8 @@ public class ClanTagsImpl
 	{
 	}
 	
-	public List<Database> databases = new ArrayList<>();;
+	public List<Database> databases = new ArrayList<>();
+	public HashMap<EntityPlayer, Faction> factionCache = new HashMap<>();
 	
 	/**
 	 * Fetches users from the database
@@ -57,7 +62,47 @@ public class ClanTagsImpl
 		debugDatabases();
 	}
 	
-	void debugDatabases()
+	public void setNameTag(EntityPlayer player)
+	{
+		Faction faction = getFaction(player);
+		if(faction != null)
+		{
+			player.addPrefix(new TextComponentString("[" + faction.tag + "]"));
+		}
+	}
+	
+	/**
+	 * Returns the {@link Faction} that an {@link EntityPlayer} belongs to
+	 * @param player the player
+	 * @return the faction the player belongs to
+	 */
+	@Nullable
+	public Faction getFaction(EntityPlayer player)
+	{
+		// Try retrieving from cache first
+		try { return Objects.requireNonNull(factionCache.get(player)); } catch(Exception ignored){}
+		
+		for(Database database : databases)
+		{
+			for(Faction faction : database.factions)
+			{
+				for(Member member : faction.members)
+				{
+					for(String alias : member.aliases)
+					{
+						if(alias.equalsIgnoreCase(player.getName()))
+						{
+							return faction;
+						}
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	private void debugDatabases()
 	{
 		for(Database database : databases)
 		{
